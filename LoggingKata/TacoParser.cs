7e86f64c2c8f94a8cbb7e75e568bf3b453d0c19d
Mohammad Jabir <mohammadj94@gmail.com>
2024-06-27 00:29:15 -0600

    code fully functional. able to find two locations with the furthest distance successfully

--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -13,47 +13,25 @@ public class TacoParser
 
     public ITrackable Parse(string line)
     {
-        //logger.LogInfo("Begin parsing");
-
-        // Take your line and use line.Split(',') to split it up into an array of strings, separated by the char ','
         var cells = line.Split(',');
 
-        // If your array's Length is less than 3, something went wrong
         if (cells.Length < 3)
         {
-            // Log error message and return null
             logger.LogError("Array length is less than 3");
             return null;
         }
 
-        // TODO: Grab the latitude from your array at index 0
-        // You're going to need to parse your string as a `double`
-        // which is similar to parsing a string as an `int`
-        double latitude = double.Parse(cells[0]);
+        string name = cells[0];
+        double latitude = double.Parse(cells[1]);
+        double longitude = double.Parse(cells[2]);
 
-        // TODO: Grab the longitude from your array at index 1
-        // You're going to need to parse your string as a `double`
-        // which is similar to parsing a string as an `int`
-        double longitude = double.Parse(cells[1]);
-
-        // TODO: Grab the name from your array at index 2
-        string name = cells[2];
-
-        // TODO: Create a TacoBell class
-        // that conforms to ITrackable
-
-        // TODO: Create an instance of the Point Struct
         Point point = new Point();
-        // TODO: Set the values of the point correctly (Latitude and Longitude) 
         point.Latitude = latitude;
         point.Longitude = longitude;
-        // TODO: Create an instance of the TacoBell class
+
         TacoBellLocation tacoBell = new TacoBellLocation();
-        // TODO: Set the values of the class correctly (Name and Location)
         tacoBell.Name = name;
         tacoBell.Location = point;
-        // TODO: Then, return the instance of your TacoBell class,
-        // since it conforms to ITrackable
 
         return tacoBell;
     }
